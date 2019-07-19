d1f3b3abf096121fb2bbc950d179276755f6cded
Brice Diomande <Bricediomande@gmal.com>
2019-07-19 13:50:01 -0400

    -add data persistence. The task will remain in the app with user default method

--- todoey/ToDoListViewController.swift
+++ todoey/ToDoListViewController.swift
@@ -12,9 +12,15 @@ class ToDoListViewController: UITableViewController {
     
     var itemArray = ["grocery","find mike", "eat the cat"]
 
+    
+    let defaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        if let items = defaults.array(forKey: "ToDoListArray") as? [String]{
+        itemArray = items
+        }
     }
     
     //MARK - Tableview Datasource Methods
@@ -55,6 +61,8 @@ class ToDoListViewController: UITableViewController {
         let action = UIAlertAction(title: "Add Item",style: .default) {(action) in
             if textField.text != ""{
             self.itemArray.append(textField.text!)
+                
+                self.defaults.set(self.itemArray, forKey: "ToDoListArray")
                 self.tableView.reloadData()
             }
         }
